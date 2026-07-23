efce93fc0e96405d01429e8af65ad186d09ca884
josue santos <josue.santos.dev@gmail.com>
2026-07-23 15:52:35 -0300

    lint fix ignore

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,6 +11,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
+// eslint-disable-next-line vue/multi-word-component-names
 app.component('Toaster', Toaster)
 
 app.mount('#app')
